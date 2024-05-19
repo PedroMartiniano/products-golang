a059f6ac4c9006778402a568ff90c3fba3c3dc8b
PedroMartiniano <110617020+PedroMartiniano@users.noreply.github.com>
2024-05-19 01:43:54 -0300

    update swagger infos

--- config/swagger.go
+++ config/swagger.go
@@ -6,9 +6,9 @@ func SwaggerConfig(config *swag.Spec) {
 	deploy_url := GetEnv("DEPLOY_URL")
 
 	config.Title = "API Products"
-	config.Description = "Documentação pública de uma api de product feita em golang"
+	config.Description = "Documentação pública da api de products feita em Golang por Pedro Paulino Martiniano"
 	config.Version = "1.0.0"
 	config.Host = deploy_url
 	config.BasePath = "/"
-	config.Schemes = []string{"http", "https"}
+	config.Schemes = []string{"https", "http"}
 }
