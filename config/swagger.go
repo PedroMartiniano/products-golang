683cbdae0f655c7169cf55568f0a70458adea489
PedroMartiniano <110617020+PedroMartiniano@users.noreply.github.com>
2024-05-19 01:35:14 -0300

    update docs to work on production

--- config/swagger.go
+++ config/swagger.go
@@ -3,10 +3,12 @@ package config
 import "github.com/swaggo/swag"
 
 func SwaggerConfig(config *swag.Spec) {
+	deploy_url := GetEnv("DEPLOY_URL")
+
 	config.Title = "API Products"
 	config.Description = "Documentação pública de uma api de product feita em golang"
 	config.Version = "1.0.0"
-	config.Host = "localhost:8080"
+	config.Host = deploy_url
 	config.BasePath = "/"
 	config.Schemes = []string{"http", "https"}
 }
